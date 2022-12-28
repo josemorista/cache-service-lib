a67f4d8548c1ba607814398aced351a9a4dee07d
josemorista <josemorista@id.uff.br>
2022-12-28 20:52:53 -0300

    feat: exported types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Types
+export { CacheServiceProtocol as CacheService } from './services/CacheServiceProtocol';
+
+// Strategies
 export { MemCacheStrategy } from './strategies/MemCacheStrategy';
 export { RedisCacheStrategy } from './strategies/RedisCacheStrategy';
 export { DynamoDbCacheStrategy } from './strategies/DynamoDbCacheStrategy';
@@ -8,33 +12,33 @@ import { CacheServiceProtocol } from './services/CacheServiceProtocol';
 import { SyncCacheService } from './services/SyncCacheService';
 
 interface CreateCacheServiceParams {
-	strategyStorage?: 'sync' | 'async';
+  strategyStorage?: 'sync' | 'async';
 }
 
 /**
  * Utility times
  */
 export const COMMON_TIMES = {
-	ONE_DAY: 86400,
-	ONE_HOUR: 3600,
-	HALF_HOUR: 1800,
-	FIFTEEN_MINUTES: 900,
-	ONE_MINUTE: 60,
+  ONE_DAY: 86400,
+  ONE_HOUR: 3600,
+  HALF_HOUR: 1800,
+  FIFTEEN_MINUTES: 900,
+  ONE_MINUTE: 60,
 };
 
 /**
  * Factories
  */
 export const createCacheService = ({ strategyStorage }: CreateCacheServiceParams = {}) => {
-	let cacheService: CacheServiceProtocol = new SyncCacheService();
-	if (strategyStorage === 'async') {
-		cacheService = new AsyncCacheService();
-	}
-	return cacheService;
+  let cacheService: CacheServiceProtocol = new SyncCacheService();
+  if (strategyStorage === 'async') {
+    cacheService = new AsyncCacheService();
+  }
+  return cacheService;
 };
 
 export const plugins = {
-	autoRefresh: (cacheService: CacheServiceProtocol) => {
-		return new AutoRefreshCache(cacheService);
-	},
+  autoRefresh: (cacheService: CacheServiceProtocol) => {
+    return new AutoRefreshCache(cacheService);
+  },
 };
